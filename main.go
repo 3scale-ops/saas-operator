b81f6f0796463acc668c1ca508e6b6c1d5de53e6
Sergio López <slopezma@redhat.com>
2021-02-02 18:14:31 +0100

    Bootrstrap Backend. Add API definition + make manigests/generate

--- main.go
+++ main.go
@@ -144,6 +144,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CORSProxy")
 		os.Exit(1)
 	}
+	if err = (&controllers.BackendReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Backend"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Backend")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
