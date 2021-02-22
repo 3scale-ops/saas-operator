a748f27602e9e2b2340550e4cc0ab4dfe8abdc53
Sergio López <slopezma@redhat.com>
2021-02-22 13:26:19 +0100

    Add Zync controller

--- controllers/zync_controller.go
+++ controllers/zync_controller.go
@@ -18,39 +18,138 @@ package controllers
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/go-logr/logr"
-	"k8s.io/apimachinery/pkg/runtime"
+	"github.com/redhat-cop/operator-utils/pkg/util"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	saasv1alpha1 "github.com/3scale/saas-operator/api/v1alpha1"
+	"github.com/3scale/saas-operator/pkg/basereconciler"
+	"github.com/3scale/saas-operator/pkg/generators/zync"
 )
 
 // ZyncReconciler reconciles a Zync object
 type ZyncReconciler struct {
-	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	basereconciler.Reconciler
+	Log logr.Logger
 }
 
-// +kubebuilder:rbac:groups=saas.3scale.net,resources=zyncs,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=saas.3scale.net,resources=zyncs/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=saas.3scale.net,resources=zyncs/finalizers,verbs=update
+// +kubebuilder:rbac:groups=saas.3scale.net,namespace=placeholder,resources=zyncs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=saas.3scale.net,namespace=placeholder,resources=zyncs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=saas.3scale.net,namespace=placeholder,resources=zyncs/finalizers,verbs=update
+// +kubebuilder:rbac:groups="core",namespace=placeholder,resources=services,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="core",namespace=placeholder,resources=secrets,verbs=get;list;watch
+// +kubebuilder:rbac:groups="apps",namespace=placeholder,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="monitoring.coreos.com",namespace=placeholder,resources=podmonitors,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="autoscaling",namespace=placeholder,resources=horizontalpodautoscalers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="policy",namespace=placeholder,resources=poddisruptionbudgets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="integreatly.org",namespace=placeholder,resources=grafanadashboards,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="secrets-manager.tuenti.io",namespace=placeholder,resources=secretdefinitions,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Zync object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
 func (r *ZyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = r.Log.WithValues("zync", req.NamespacedName)
+	log := r.Log.WithValues("name", req.Name, "namespace", req.Namespace)
 
-	// your logic here
+	instance := &saasv1alpha1.Zync{}
+	key := types.NamespacedName{Name: req.Name, Namespace: req.Namespace}
+	result, err := r.GetInstance(ctx, key, instance, saasv1alpha1.Finalizer, log)
+	if result != nil || err != nil {
+		return *result, err
+	}
+
+	// Apply defaults for reconcile but do not store them in the API
+	instance.Default()
+	json, _ := json.Marshal(instance.Spec)
+	log.V(1).Info("Apply defaults before resolving templates", "JSON", string(json))
+
+	gen := zync.NewGenerator(
+		instance.GetName(),
+		instance.GetNamespace(),
+		instance.Spec,
+	)
+
+	// Calculate rollout triggers
+	triggers, err := r.TriggersFromSecretDefs(ctx,
+		gen.ZyncSecretDefinition(),
+	)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	err = r.ReconcileOwnedResources(ctx, instance, basereconciler.ControlledResources{
+		Deployments: []basereconciler.Deployment{
+			{
+				Template:        gen.API.Deployment(),
+				HasHPA:          !instance.Spec.API.HPA.IsDeactivated(),
+				RolloutTriggers: triggers,
+			},
+			{
+				Template:        gen.Que.Deployment(),
+				HasHPA:          !instance.Spec.Que.HPA.IsDeactivated(),
+				RolloutTriggers: triggers,
+			},
+		},
+		SecretDefinitions: []basereconciler.SecretDefinition{
+			{
+				Template: gen.ZyncSecretDefinition(),
+				Enabled:  true,
+			},
+		},
+		Services: []basereconciler.Service{
+			{
+				Template: gen.API.Service(),
+				Enabled:  true,
+			}},
+		PodDisruptionBudgets: []basereconciler.PodDisruptionBudget{
+			{
+				Template: gen.API.PDB(), // Calculate rollout triggers
+				Enabled:  !instance.Spec.API.PDB.IsDeactivated(),
+			},
+			{
+				Template: gen.Que.PDB(), // Calculate rollout triggers
+				Enabled:  !instance.Spec.Que.PDB.IsDeactivated(),
+			},
+		},
+		HorizontalPodAutoscalers: []basereconciler.HorizontalPodAutoscaler{
+			{
+				Template: gen.API.HPA(),
+				Enabled:  !instance.Spec.API.HPA.IsDeactivated(),
+			},
+			{
+				Template: gen.Que.HPA(),
+				Enabled:  !instance.Spec.Que.HPA.IsDeactivated(),
+			},
+		},
+		PodMonitors: []basereconciler.PodMonitor{
+			{
+				Template: gen.API.PodMonitor(),
+				Enabled:  true,
+			},
+			{
+				Template: gen.Que.PodMonitor(),
+				Enabled:  true,
+			},
+		},
+		GrafanaDashboards: []basereconciler.GrafanaDashboard{
+			{
+				Template: gen.GrafanaDashboard(),
+				Enabled:  !instance.Spec.GrafanaDashboard.IsDeactivated(),
+			},
+		},
+	})
+
+	if err != nil {
+		log.Error(err, "unable to reconcile owned resources")
+		return r.ManageError(ctx, instance, err)
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -58,6 +157,9 @@ func (r *ZyncReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 // SetupWithManager sets up the controller with the Manager.
 func (r *ZyncReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&saasv1alpha1.Zync{}).
+		For(&saasv1alpha1.Zync{}, builder.WithPredicates(util.ResourceGenerationOrFinalizerChangedPredicate{})).
+		Watches(&source.Channel{Source: r.GetStatusChangeChannel()}, &handler.EnqueueRequestForObject{}).
+		Watches(&source.Kind{Type: &corev1.Secret{TypeMeta: metav1.TypeMeta{Kind: "Secret"}}},
+			r.SecretEventHandler(&saasv1alpha1.ZyncList{}, r.Log)).
 		Complete(r)
 }
---
+++ controllers/zync_controller_suite_test.go
@@ -0,0 +1,187 @@
+package controllers
+
+import (
+	"context"
+
+	saasv1alpha1 "github.com/3scale/saas-operator/api/v1alpha1"
+	grafanav1alpha1 "github.com/3scale/saas-operator/pkg/apis/grafana/v1alpha1"
+	secretsmanagerv1alpha1 "github.com/3scale/saas-operator/pkg/apis/secrets-manager/v1alpha1"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv2beta2 "k8s.io/api/autoscaling/v2beta2"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	policyv1beta1 "k8s.io/api/policy/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("Zync controller", func() {
+	var namespace string
+	var zync *saasv1alpha1.Zync
+
+	BeforeEach(func() {
+		// Create a namespace for each block
+		namespace = "test-ns-" + nameGenerator.Generate()
+
+		// Add any setup steps that needs to be executed before each test
+		testNamespace := &v1.Namespace{
+			TypeMeta:   metav1.TypeMeta{APIVersion: "v1", Kind: "Namespace"},
+			ObjectMeta: metav1.ObjectMeta{Name: namespace},
+		}
+
+		err := k8sClient.Create(context.Background(), testNamespace)
+		Expect(err).ToNot(HaveOccurred())
+
+		n := &v1.Namespace{}
+		Eventually(func() error {
+			return k8sClient.Get(context.Background(), types.NamespacedName{Name: namespace}, n)
+		}, timeout, poll).ShouldNot(HaveOccurred())
+
+	})
+
+	Context("All defaults Zync resource", func() {
+
+		BeforeEach(func() {
+			By("creating a Zync simple resource")
+			zync = &saasv1alpha1.Zync{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "instance",
+					Namespace: namespace,
+				},
+				Spec: saasv1alpha1.ZyncSpec{
+					Config: saasv1alpha1.ZyncConfig{
+						DatabaseDSN: saasv1alpha1.SecretReference{
+							FromVault: &saasv1alpha1.VaultSecretReference{
+								Path: "some-path",
+								Key:  "some-key",
+							},
+						},
+						SecretKeyBase: saasv1alpha1.SecretReference{
+							FromVault: &saasv1alpha1.VaultSecretReference{
+								Path: "some-path",
+								Key:  "some-key",
+							},
+						},
+						ZyncAuthToken: saasv1alpha1.SecretReference{
+							FromVault: &saasv1alpha1.VaultSecretReference{
+								Path: "some-path",
+								Key:  "some-key",
+							},
+						},
+					},
+				},
+			}
+			err := k8sClient.Create(context.Background(), zync)
+			Expect(err).ToNot(HaveOccurred())
+			Eventually(func() error {
+				return k8sClient.Get(context.Background(), types.NamespacedName{Name: "instance", Namespace: namespace}, zync)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+		})
+
+		It("creates the required resources", func() {
+
+			Eventually(func() bool {
+				err := k8sClient.Get(context.Background(), types.NamespacedName{Name: "instance", Namespace: namespace}, zync)
+				Expect(err).ToNot(HaveOccurred())
+				if len(zync.GetFinalizers()) > 0 {
+					return true
+				}
+				return false
+			}, timeout, poll).Should(BeTrue())
+
+			dep := &appsv1.Deployment{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					dep,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync-que", Namespace: namespace},
+					dep,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+
+			svc := &corev1.Service{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					svc,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+
+			pm := &monitoringv1.PodMonitor{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					pm,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync-que", Namespace: namespace},
+					pm,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+
+			hpa := &autoscalingv2beta2.HorizontalPodAutoscaler{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					hpa,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync-que", Namespace: namespace},
+					hpa,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+
+			pdb := &policyv1beta1.PodDisruptionBudget{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					pdb,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync-que", Namespace: namespace},
+					pdb,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+
+			sd := &secretsmanagerv1alpha1.SecretDefinition{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					sd,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+
+			gd := &grafanav1alpha1.GrafanaDashboard{}
+			Eventually(func() error {
+				return k8sClient.Get(
+					context.Background(),
+					types.NamespacedName{Name: "zync", Namespace: namespace},
+					gd,
+				)
+			}, timeout, poll).ShouldNot(HaveOccurred())
+		})
+	})
+})
