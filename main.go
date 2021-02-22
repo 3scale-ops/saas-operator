7e796503799ae14eb7ed01f9413be414b7407186
Sergio López <slopezma@redhat.com>
2021-02-22 10:52:30 +0100

    Bootstrap Zync. Add API definition + make manigests/generate

--- main.go
+++ main.go
@@ -164,6 +164,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.ZyncReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Zync"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Zync")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
