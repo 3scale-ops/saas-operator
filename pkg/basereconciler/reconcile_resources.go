e5bb7529c3a7c6d8f14e44abf92032690b9a8943
Roi Vazquez <roivazquez@gmail.com>
2021-12-30 18:45:10 +0100

    Fix Services reconcile due to runtime calculated fields in the Service spec

--- pkg/basereconciler/reconcile_resources.go
+++ pkg/basereconciler/reconcile_resources.go
@@ -169,6 +169,58 @@ type Service struct {
 	Enabled  bool
 }
 
+func (s *Service) PopulateSpecRuntimeValues(ctx context.Context, cl client.Client) (basereconciler_types.GeneratorFunction, error) {
+
+	svc := s.Template().(*corev1.Service)
+	instance := &corev1.Service{}
+	if err := cl.Get(ctx, types.NamespacedName{
+		Name:      svc.GetName(),
+		Namespace: svc.GetNamespace(),
+	}, instance); err != nil {
+		if errors.IsNotFound(err) {
+			// Resource not found, return the template as is
+			// because there are not runtime values yet
+			return s.Template, nil
+		}
+		return s.Template, err
+	}
+
+	// Set runtime values in the resource:
+	// "/spec/clusterIP", "/spec/clusterIPs", "/spec/ipFamilies", "/spec/ipFamilyPolicy", "/spec/ports/*/nodePort"
+	svc.Spec.ClusterIP = instance.Spec.ClusterIP
+	svc.Spec.ClusterIPs = instance.Spec.ClusterIPs
+	svc.Spec.IPFamilies = instance.Spec.IPFamilies
+	svc.Spec.IPFamilyPolicy = instance.Spec.IPFamilyPolicy
+
+	// For services that are not ClusterIP we need to populate the runtime values
+	// of NodePort for each port
+	if svc.Spec.Type != corev1.ServiceTypeClusterIP {
+		for idx, port := range svc.Spec.Ports {
+			runtimePort := findPort(port.Port, port.Protocol, instance.Spec.Ports)
+			if runtimePort != nil {
+				svc.Spec.Ports[idx].NodePort = runtimePort.NodePort
+			}
+		}
+	}
+
+	return func() client.Object {
+		return svc
+	}, nil
+}
+
+func findPort(pNumber int32, pProtocol corev1.Protocol, ports []corev1.ServicePort) *corev1.ServicePort {
+	// Ports within a svc are uniquely identified by
+	// the "port" and "protocol" fields. This is documented in
+	// k8s API reference
+	for _, port := range ports {
+		if pNumber == port.Port && pProtocol == port.Protocol {
+			return &port
+		}
+	}
+	// not found
+	return nil
+}
+
 // PodDisruptionBudget specifies a PodDisruptionBudget resource
 type PodDisruptionBudget struct {
 	Template basereconciler_types.GeneratorFunction
@@ -272,9 +324,13 @@ func (r *Reconciler) ReconcileOwnedResources(ctx context.Context, owner client.O
 
 	for _, svc := range crs.Services {
 		if svc.Enabled {
+			template, err := svc.PopulateSpecRuntimeValues(ctx, r.GetClient())
+			if err != nil {
+				return err
+			}
 			resources = append(resources,
 				basereconciler_types.LockedResource{
-					GeneratorFn:  svc.Template,
+					GeneratorFn:  template,
 					ExcludePaths: append(DefaultExcludedPaths, ServiceExcludes(svc.Template)...),
 				})
 		}
