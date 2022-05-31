1ca76c05903c64f96041d9448f3d974aaeeea512
Roi Vazquez <roivazquez@gmail.com>
2022-05-31 11:48:41 +0200

    Cleanup dead code

--- pkg/generators/base_options.go
+++ pkg/generators/base_options.go
@@ -2,7 +2,6 @@ package generators
 
 import (
 	"github.com/3scale/saas-operator/pkg/util"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
@@ -10,60 +9,6 @@ const (
 	// PodSelectorKey is the label key used as Pod selector
 	PodSelectorKey = "deployment"
 )
-
-// BaseOptions configures the generators for a component
-type BaseOptions struct {
-	Component    string
-	InstanceName string
-	Namespace    string
-	Labels       map[string]string
-}
-
-// GetComponent returns the name of the component
-func (bo *BaseOptions) GetComponent() string {
-	return bo.Component
-}
-
-// GetInstanceName returns the name of the custom resource instance
-func (bo *BaseOptions) GetInstanceName() string {
-	return bo.InstanceName
-}
-
-// GetNamespace returns the custom resource namespace
-func (bo *BaseOptions) GetNamespace() string {
-	return bo.Namespace
-}
-
-// GetLabels returns metadata labels
-func (bo *BaseOptions) GetLabels() map[string]string {
-	// return a copy of the map and not a reference
-	m := map[string]string{}
-	for k, v := range bo.Labels {
-		m[k] = v
-	}
-	return m
-}
-
-// Key returns a types.NamespacedName
-func (bo *BaseOptions) Key() types.NamespacedName {
-	return types.NamespacedName{Name: bo.GetComponent(), Namespace: bo.GetNamespace()}
-}
-
-// LabelsWithSelector returns Labels() with the addition of the Pod
-// selector label
-func (bo *BaseOptions) LabelsWithSelector() map[string]string {
-	labels := bo.GetLabels()
-	labels[PodSelectorKey] = bo.GetComponent()
-	return labels
-}
-
-// Selector returns the LabelSelector struct that matches the labels in
-// LabelsWithSelector()
-func (bo *BaseOptions) Selector() *metav1.LabelSelector {
-	return &metav1.LabelSelector{
-		MatchLabels: map[string]string{PodSelectorKey: bo.GetComponent()},
-	}
-}
 
 // BaseOptions configures the generators for a component
 type BaseOptionsV2 struct {
