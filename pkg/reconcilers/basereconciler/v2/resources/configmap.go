8cc257989862736d9edd350d2be32d4d39c0d767
Roi Vazquez <roivazquez@gmail.com>
2022-09-22 21:26:22 +0200

    Add reconciler for ConfigMap resources

---
+++ pkg/reconcilers/basereconciler/v2/resources/configmap.go
@@ -0,0 +1,93 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+
+	basereconciler "github.com/3scale/saas-operator/pkg/reconcilers/basereconciler/v2"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+var _ basereconciler.Resource = ConfigMapTemplate{}
+
+// ConfigMaps specifies a ConfigMap resource
+type ConfigMapTemplate struct {
+	Template  func() *corev1.ConfigMap
+	IsEnabled bool
+}
+
+func (cmt ConfigMapTemplate) Build(ctx context.Context, cl client.Client) (client.Object, []string, error) {
+
+	cm := cmt.Template()
+	cm.GetObjectKind().SetGroupVersionKind(corev1.SchemeGroupVersion.WithKind("ConfigMap"))
+	return cm.DeepCopy(), DefaultExcludedPaths, nil
+}
+
+func (cmt ConfigMapTemplate) Enabled() bool {
+	return cmt.IsEnabled
+}
+
+func (cmt ConfigMapTemplate) ResourceReconciler(ctx context.Context, cl client.Client, obj client.Object) error {
+	logger := log.FromContext(ctx, "ResourceReconciler", "ConfigMap")
+
+	needsUpdate := false
+	desired := obj.(*corev1.ConfigMap)
+
+	instance := &corev1.ConfigMap{}
+	err := cl.Get(ctx, types.NamespacedName{Name: desired.GetName(), Namespace: desired.GetNamespace()}, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+
+			if cmt.Enabled() {
+				err = cl.Create(ctx, desired)
+				if err != nil {
+					return fmt.Errorf("unable to create object: " + err.Error())
+				}
+				logger.Info("Resource created")
+				return nil
+
+			} else {
+				return nil
+			}
+		}
+
+		return err
+	}
+
+	/* Delete and return if not enabled */
+	if !cmt.Enabled() {
+		err := cl.Delete(ctx, instance)
+		if err != nil {
+			return fmt.Errorf("unable to delete object: " + err.Error())
+		}
+		logger.Info("Resource deleted")
+		return nil
+	}
+
+	/* Reconcile metadata */
+	if !equality.Semantic.DeepEqual(instance.GetLabels(), desired.GetLabels()) {
+		instance.ObjectMeta.Labels = desired.GetLabels()
+		needsUpdate = true
+	}
+
+	/* Reconcile the data */
+	if !equality.Semantic.DeepEqual(instance.Data, desired.Data) {
+		instance.Data = desired.Data
+		needsUpdate = true
+	}
+
+	if needsUpdate {
+		err := cl.Update(ctx, instance)
+		if err != nil {
+			return err
+		}
+		logger.Info("Resource updated")
+	}
+
+	return nil
+}
