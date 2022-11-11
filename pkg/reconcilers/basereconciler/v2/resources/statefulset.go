7111f2a0c84e5393f9d7397753148a2426bc6c0c
Rael Garcia <rael@redhat.com>
2022-11-11 16:04:38 +0000

    feat: improve sts custom reconciler logging
    Signed-off-by: Rael Garcia <rael@redhat.com>

--- pkg/reconcilers/basereconciler/v2/resources/statefulset.go
+++ pkg/reconcilers/basereconciler/v2/resources/statefulset.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	basereconciler "github.com/3scale/saas-operator/pkg/reconcilers/basereconciler/v2"
+	"github.com/go-test/deep"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -53,7 +54,10 @@ func (sst StatefulSetTemplate) Enabled() bool {
 }
 
 func (sts StatefulSetTemplate) ResourceReconciler(ctx context.Context, cl client.Client, obj client.Object) error {
-	logger := log.FromContext(ctx, "ResourceReconciler", "StatefulSet")
+	logger := log.FromContext(ctx,
+		"ResourceReconciler", "StatefulSet",
+		"Resource", obj.GetName(),
+	)
 
 	needsUpdate := false
 	desired := obj.(*appsv1.StatefulSet)
@@ -91,40 +95,75 @@ func (sts StatefulSetTemplate) ResourceReconciler(ctx context.Context, cl client
 
 	/* Reconcile metadata */
 	if !equality.Semantic.DeepEqual(instance.GetAnnotations(), desired.GetAnnotations()) {
+		logger.Info("Resource update required due differences in Annotations.")
+		logger.V(1).Info(
+			fmt.Sprintf("Annotations differences: %s",
+				deep.Equal(instance.GetAnnotations(), desired.GetAnnotations())),
+		)
 		instance.ObjectMeta.Annotations = desired.GetAnnotations()
 		needsUpdate = true
 	}
 	if !equality.Semantic.DeepEqual(instance.GetLabels(), desired.GetLabels()) {
+		logger.Info("Resource update required due differences in Labels.")
+		logger.V(1).Info(
+			fmt.Sprintf("Labels differences: %s",
+				deep.Equal(instance.GetLabels(), desired.GetLabels())),
+		)
 		instance.ObjectMeta.Labels = desired.GetLabels()
 		needsUpdate = true
 	}
 
 	/* Reconcile the MinReadySeconds */
 	if !equality.Semantic.DeepEqual(instance.Spec.MinReadySeconds, desired.Spec.MinReadySeconds) {
+		logger.Info("Resource update required due differences in Spec.MinReadySeconds.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.MinReadySeconds differences: %s",
+				deep.Equal(instance.Spec.MinReadySeconds, desired.Spec.MinReadySeconds)),
+		)
 		instance.Spec.MinReadySeconds = desired.Spec.MinReadySeconds
 		needsUpdate = true
 	}
 
 	/* Reconcile the PersistentVolumeClaimRetentionPolicy */
 	if !equality.Semantic.DeepEqual(instance.Spec.PersistentVolumeClaimRetentionPolicy, desired.Spec.PersistentVolumeClaimRetentionPolicy) {
+		logger.Info("Resource update required due differences in Spec.PersistentVolumeClaimRetentionPolicy.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.PersistentVolumeClaimRetentionPolicy differences: %s",
+				deep.Equal(instance.Spec.PersistentVolumeClaimRetentionPolicy, desired.Spec.PersistentVolumeClaimRetentionPolicy)),
+		)
 		instance.Spec.PersistentVolumeClaimRetentionPolicy = desired.Spec.PersistentVolumeClaimRetentionPolicy
 		needsUpdate = true
 	}
 
 	/* Reconcile the Replicas */
 	if !equality.Semantic.DeepEqual(instance.Spec.Replicas, desired.Spec.Replicas) {
+		logger.Info("Resource update required due differences in Spec.Replicas.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.Replicas differences: %s",
+				deep.Equal(instance.Spec.Replicas, desired.Spec.Replicas)),
+		)
 		instance.Spec.Replicas = desired.Spec.Replicas
 		needsUpdate = true
 	}
 
 	/* Reconcile the Selector */
 	if !equality.Semantic.DeepEqual(instance.Spec.Selector, desired.Spec.Selector) {
+		logger.Info("Resource update required due differences in Spec.Selector.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.Selector differences: %s",
+				deep.Equal(instance.Spec.Selector, desired.Spec.Selector)),
+		)
 		instance.Spec.Selector = desired.Spec.Selector
 		needsUpdate = true
 	}
 
 	/* Reconcile the ServiceName */
 	if !equality.Semantic.DeepEqual(instance.Spec.ServiceName, desired.Spec.ServiceName) {
+		logger.Info("Resource update required due differences in Spec.ServiceName.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.ServiceName differences: %s",
+				deep.Equal(instance.Spec.ServiceName, desired.Spec.ServiceName)),
+		)
 		instance.Spec.ServiceName = desired.Spec.ServiceName
 		needsUpdate = true
 	}
@@ -132,24 +171,44 @@ func (sts StatefulSetTemplate) ResourceReconciler(ctx context.Context, cl client
 	/* Reconcile the Template Labels */
 	if !equality.Semantic.DeepEqual(
 		instance.Spec.Template.ObjectMeta.Labels, desired.Spec.Template.ObjectMeta.Labels) {
+		logger.Info("Resource update required due differences in Spec.Template.ObjectMeta.Labels.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.Template.ObjectMeta.Labels differences: %s",
+				deep.Equal(instance.Spec.Template.ObjectMeta.Labels, desired.Spec.Template.ObjectMeta.Labels)),
+		)
 		instance.Spec.Template.ObjectMeta.Labels = desired.Spec.Template.ObjectMeta.Labels
 		needsUpdate = true
 	}
 
 	/* Reconcile the Template Spec */
 	if !equality.Semantic.DeepEqual(instance.Spec.Template.Spec, desired.Spec.Template.Spec) {
+		logger.Info("Resource update required due differences in Spec.Template.Spec.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.Template.Spec differences: %s",
+				deep.Equal(instance.Spec.Template.Spec, desired.Spec.Template.Spec)),
+		)
 		instance.Spec.Template.Spec = desired.Spec.Template.Spec
 		needsUpdate = true
 	}
 
 	/* Reconcile the UpdateStrategy */
 	if !equality.Semantic.DeepEqual(instance.Spec.UpdateStrategy, desired.Spec.UpdateStrategy) {
+		logger.Info("Resource update required due differences in Spec.UpdateStrategy.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.UpdateStrategy differences: %s",
+				deep.Equal(instance.Spec.UpdateStrategy, desired.Spec.UpdateStrategy)),
+		)
 		instance.Spec.UpdateStrategy = desired.Spec.UpdateStrategy
 		needsUpdate = true
 	}
 
 	/* Reconcile the VolumeClaimTemplates */
 	if !equality.Semantic.DeepEqual(instance.Spec.VolumeClaimTemplates, desired.Spec.VolumeClaimTemplates) {
+		logger.Info("Resource update required due differences in Spec.VolumeClaimTemplates.")
+		logger.V(1).Info(
+			fmt.Sprintf("Spec.VolumeClaimTemplates differences: %s",
+				deep.Equal(instance.Spec.VolumeClaimTemplates, desired.Spec.VolumeClaimTemplates)),
+		)
 		instance.Spec.VolumeClaimTemplates = desired.Spec.VolumeClaimTemplates
 		needsUpdate = true
 	}
