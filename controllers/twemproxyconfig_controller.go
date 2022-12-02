43c428ca85726fc75493b9b489a9e52465ef1a28
Roi Vazquez <roivazquez@gmail.com>
2022-12-02 13:16:47 +0100

    Fix regression introduced by commit 8bc3e0d
    Mistakenly, the watch for sentinel events was removed from the
    TwemproxyConfig controller with commit
    8bc3e0d. This is hard to detect because
    it just make the reconcile slower so the tests pass. This commit brings
    back the watch to speed up reconcile times for twemprox configuration.

--- controllers/twemproxyconfig_controller.go
+++ controllers/twemproxyconfig_controller.go
@@ -38,7 +38,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // TwemproxyConfigReconciler reconciles a TwemproxyConfig object
@@ -230,5 +232,6 @@ func (r *TwemproxyConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&saasv1alpha1.TwemproxyConfig{}).
 		Owns(&corev1.ConfigMap{}).
 		Owns(&grafanav1alpha1.GrafanaDashboard{}).
+		Watches(&source.Channel{Source: r.SentinelEvents.GetChannel()}, &handler.EnqueueRequestForObject{}).
 		Complete(r)
 }
