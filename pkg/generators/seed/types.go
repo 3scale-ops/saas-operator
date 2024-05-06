01bd15f373b3dea3b198a910fcf99b480bfdc656
Roi Vazquez <roivazquez@gmail.com>
2024-05-06 16:46:53 +0200

    Clean pkg/generators/seed/types.go

--- pkg/generators/seed/types.go
+++ pkg/generators/seed/types.go
@@ -15,7 +15,7 @@ const (
 	SystemDatabaseDsn                  SeedKey = "system-database-dsn"
 	SystemRecaptchaPublicKey           SeedKey = "system-recaptcha-public-key"
 	SystemRecaptchaPrivateKey          SeedKey = "system-recaptcha-private-key"
-	SystemEventsHookURL                SeedKey = "system-events-url" // this shouldn't be a secret
+	SystemEventsHookURL                SeedKey = "system-events-url"
 	SystemEventsHookSharedSecret       SeedKey = "system-events-shared-secret"
 	SystemSmtpUser                     SeedKey = "system-smpt-user"
 	SystemSmtpPassword                 SeedKey = "system-smpt-password"
@@ -40,21 +40,3 @@ const (
 	ZyncAuthToken     SeedKey = "zync-auth-token"
 	ZyncBugsnagApiKey SeedKey = "zync-bugsnag-api-key"
 )
-
-// TODO: use this to generate a Secret from some input params
-// var AutoGen map[SeedKey]string = map[SeedKey]string{
-// 	BackendInternalApiUser:       "user",
-// 	BackendInternalApiPassword:   "<generate#backend-internal-api-password>",
-// 	SystemDatabaseDsn:            "mysql2://app:<generate#system-db-password>@<param#system-db-host>:3306/system_enterprise",
-// 	SystemEventsHookURL:          "https://<param#system-app-host>/master/events/import",
-// 	SystemEventsHookSharedSecret: "<generate#system-events-shared-secret>",
-// 	SystemMasterAccessToken:      "<generate#system-master-token>",
-// 	SystemAssetsS3AwsAccessKey:   "<param#s3-aws-access-key>",
-// 	SystemAssetsS3AwsSecretKey:   "<param#s3-aws-secret-key>",
-// 	SystemSecretKeyBase:          "<generate#system-key-base>",
-// 	SystemAccessCode:             "<generate#system-access-code>",
-// 	SystemDatabaseSecret:         "<generate#system-db-secret>",
-// 	ZyncDatabaseUrl:              "postgresql://app:<generate#zync-db-password>@<param#zync-db-host>:5432/zync",
-// 	ZyncSecretKeyBase:            "<generate#zync-secret-key-base>",
-// 	ZyncAuthToken:                "<generate#zync-auth-token>",
-// }
